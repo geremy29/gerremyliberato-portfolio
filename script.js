70fe7b194c3dd87b3ed321ee0fd6b59f11d05a9c
geremy29 <geremyliberato67@gmail.com>
2024-08-05 13:30:18 +0000

    I let JavaScript join the pool party

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('hello world')
+
+// var prompt= prompt('what is your name?');
+
+console.log(prompt)
+
+// if(10>9){
+// console.log('yes ten is greater than 9');
+// }else{
+//   console.log('nuh uh')
+// }
+
+
+var age= prompt('what is your age?');
+
+if (age < 18 && typeof age ==="string") {
+  alert('goo goo gaa gaa?')
+} else if (age>=18 && age <=60){
+  alert('im fine with that')
+} else {
+  alert('what were the dinosaurs like?')
+}
